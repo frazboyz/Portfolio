8e3d92965a8b384bb6de5b8e43186a9a0ada46a0
francis <frazboyz101@gmail.com>
2015-11-18 18:29:37 +1300

    Made images precompile

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module CvFrancismbCom
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.assets.precompile += [/.*\.js/,/.*\.css/]
+    config.assets.precompile += [/.*\.png/,/.*\.jpg/,/.*\.jpeg/,/.*\.gif/]
+    config.assets.precompile += [/.*\.js/, /.*\.css/]
   end
 end
