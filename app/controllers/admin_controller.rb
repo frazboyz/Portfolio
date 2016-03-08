5cd06fc920c2909e1b6fb8a043f40394d1e2456a
Francis <frazboyz101@gmail.com>
2016-03-08 18:06:36 +1300

    Added bcrypt authentication to the user

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,21 +1,29 @@
 class AdminController < ApplicationController
 
   layout "admin"
-
+  
+  # Shows the form to login.
+  # GET http://francismb.com/admin/login
   def login
     if logged_in
       redirect_to :controller => :application, :action => :index
     end
   end
 
+  # Logs the user out and redirects to the home page.
+  # GET http://francismb.com/admin/logout
   def logout
     session[:user_id] = nil
     redirect_to :controller => :application
   end
 
+  # Processes the form sent by #login and sets the
+  # session id if the login gets successfully authenticated
+  # else it renders the login again.
+  # POST http://francismb.com/admin/authenticate
   def authenticate
     user = User.find_by_username(params[:username]);
-    if user && params[:password] === 'testing'
+    if user && user.authenticate(params[:password])
       flash[:error] = nil
       session[:user_id] = user.id
       redirect_to :controller => :application, :action => :index
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125051424) do
+ActiveRecord::Schema.define(version: 20160308045944) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20160125051424) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
   end
 
 end
