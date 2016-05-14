6f90fc8273a5c7c59f2dd43ba2dbea10eb224ec9
Francis <frazboyz101@gmail.com>
2016-05-14 15:52:59 +1200

    Removed unused relationships

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,8 +5,6 @@ class Project < ActiveRecord::Base
   validates :content, :presence => {:message => 'The content field cannot be blank'}
   validates :image, :presence => {:message => 'The image field cannot be blank'}
 
-  has_and_belongs_to_many :skills
-
   mount_uploader :image, ProjectImageUploader
 
 end
