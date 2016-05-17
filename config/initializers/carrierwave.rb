7352bd23913b85e3b628fac78cc47fa0409f3ff6
Francis <frazboyz101@gmail.com>
2016-05-17 20:44:50 +1200

    Moved from dropbox iamge hosting to rackspace

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,9 @@
 CarrierWave.configure do |config|
-  config.dropbox_app_key = ENV["APP_KEY"]
-  config.dropbox_app_secret = ENV["APP_SECRET"]
-  config.dropbox_access_token = ENV["ACCESS_TOKEN"]
-  config.dropbox_access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-  config.dropbox_user_id = ENV["USER_ID"]
-  config.dropbox_access_type = "app_folder"
+  config.fog_credentials = {
+      provider: 'Rackspace',
+      rackspace_username: 'frazboyz',
+      rackspace_api_key: ENV['RACKSPACE_API_KEY'],
+      rackspace_region: 'SYD'
+  }
+  config.fog_directory = 'portfolio'
 end
