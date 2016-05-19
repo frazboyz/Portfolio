d6aaef34ed2af3b97b0bf035b14269c638050df1
Francis <frazboyz101@gmail.com>
2016-05-19 20:33:36 +1200

    Vertically aligning skill summary blocks

--- app/assets/javascripts/skills.js
+++ app/assets/javascripts/skills.js
@@ -3,11 +3,18 @@
  */
 "use strict";
 $(window).load(function () {
+
     /**
      * The skills collection.
      * @type {*|jQuery|HTMLElement}
      */
     var skills = $('.skills > section');
+
+    /**
+     * The summaries collection.
+     * @type {*|jQuery|HTMLElement}
+     */
+    var summaries = $('.skills > section p');
 
     /**
      * Calculate the maximum height for the skill boxes and
@@ -19,4 +26,15 @@ $(window).load(function () {
         max_height = Math.max(max_height, $(this).height());
     });
     skills.height(max_height);
+
+    /**
+     * Calculate and set the maximum height for each of
+     * the summaries in the skill boxes.
+     * @type {number}
+     */
+    max_height = 0;
+    summaries.each(function () {
+        max_height = Math.max(max_height, $(this).height());
+    });
+    summaries.height(max_height);
 });
